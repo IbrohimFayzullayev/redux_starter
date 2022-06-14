4b38b06a3ec86a479b564b41a2a9c0502ca81459
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-06-14 23:11:13 +0500

    project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import {
+  compose,
+  bindActionCreators,
+  applyMiddleware,
+  createStore,
+} from "redux";
+// import App from "./App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+function Render() {
+  const salom = (string) => string.toUpperCase();
+  const hello = (string) => string.repeat(3);
+  const str = (string) => string.toLowerCase();
+  const mix = str(hello(salom("Ibrohim")));
+  const mixWithCompose = compose(salom, hello, str);
+  return (
+    <div>
+      {mix}
+      {mixWithCompose("Ibrohim")}
+    </div>
+  );
+}
+
+ReactDOM.render(<Render />, document.querySelector("#root"));
