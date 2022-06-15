f70593e71720bce51cac52d23d0cdbd14581a26e
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-06-15 11:55:44 +0500

    react connect redux

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ButtonCounter from "./ButtonCounter";
+import DisplayCounter from "./DisplayCounter";
+function App() {
+  return (
+    <div>
+      <DisplayCounter />
+      <ButtonCounter />
+    </div>
+  );
+}
+
+export default App;
