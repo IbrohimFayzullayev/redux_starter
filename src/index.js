8445387f6f9f7798ba4002c4294675b5e3b4943c
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-06-15 11:55:30 +0500

    react connect redux

--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-  compose,
-  bindActionCreators,
-  applyMiddleware,
-  createStore,
-} from "redux";
-// import App from "./App";
-
-function Render() {
-  const salom = (string) => string.toUpperCase();
-  const hello = (string) => string.repeat(3);
-  const str = (string) => string.toLowerCase();
-  const mix = str(hello(salom("Ibrohim")));
-  const mixWithCompose = compose(salom, hello, str);
-  return (
-    <div>
-      {mix}
-      {mixWithCompose("Ibrohim")}
-    </div>
-  );
-}
-
-ReactDOM.render(<Render />, document.querySelector("#root"));
+import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./store";
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
