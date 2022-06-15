8c42729ef0e9416da5f0cb4ffff704e71243ceb4
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-06-15 11:55:50 +0500

    react connect redux

---
+++ src/components/ButtonCounter.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import {
+  incrementAction,
+  decrementAction,
+  resetAction,
+} from "../actions/counterAction";
+
+const ButtonCounter = () => {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch(incrementAction());
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          dispatch(resetAction());
+        }}
+      >
+        Reset
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrementAction());
+        }}
+      >
+        Decrement
+      </button>
+    </div>
+  );
+};
+
+export default ButtonCounter;
