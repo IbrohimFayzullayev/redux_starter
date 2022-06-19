bbc7e7939eb765f656b2035b5107dc4961572615
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-06-19 16:31:39 +0500

    project

--- src/components/Api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const getData = async (path) => {
-  const data = await axios.get(`https://jsonplaceholder.typicode.com/${path}`);
-  return data;
-};
-
-export default getData;
